e3b1b066f205b3f7997a4bbd6c46d22a6192e3eb
joelthchao <joelthchao@gmail.com>
2015-09-08 00:01:38 +0800

    add plot

---
+++ script/plot.sh
@@ -0,0 +1,5 @@
+~/public-html/plot_log/extra/plot_training_log_joel.py 0 \
+~/public-html/static-drop.png \
+~/git_project/diverse-dropout/log/chao_test_sdfc3.log \
+~/git_project/diverse-dropout/log/chao_test_sdfc2.log \
+~/git_project/diverse-dropout/log/chao_test_sdfc1.log
