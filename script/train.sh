1565e80ffc3831e1e20a080fcc599e971d9c390c
joelthchao <joelthchao@gmail.com>
2015-09-07 21:58:24 +0800

    double check for static dropout

--- script/train.sh
+++ script/train.sh
@@ -1,1 +1,5 @@
-~/caffe/build/tools/caffe train -weights=/tmp3/joel1211/fc2conv/model/caffenet/bvlc_reference_caffenet.caffemodel -solver=/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_test_sdfc1/solver.prototxt -gpu=3 2>&1 | tee /home/master/02/joel1211/git_project/diverse-dropout/log/chao_test_sdfc1.log
+~/caffe/build/tools/caffe train \
+-weights=/tmp3/joel1211/fc2conv/model/caffenet/bvlc_reference_caffenet.caffemodel \
+-solver=/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_test_sdfc3/solver.prototxt \
+-gpu=1 \
+2>&1 | tee /home/master/02/joel1211/git_project/diverse-dropout/log/chao_test_sdfc3.log
