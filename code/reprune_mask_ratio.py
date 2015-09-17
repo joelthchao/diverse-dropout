c1bb11cf50527350e8de8bdaa1377eb4a66e02e4
joelthchao <joelthchao@gmail.com>
2015-09-17 21:20:32 +0800

    add reprune

--- code/reprune_mask_ratio.py
+++ code/reprune_mask_ratio.py
@@ -15,13 +15,16 @@ parser = argparse.ArgumentParser(description='Generate mask for pretrained model
 parser.add_argument('-ratio',type=float) # ratio of zero out
 parser.add_argument('-name',type=str) # name of layer
 parser.add_argument('-out',type=str) # output mask file
+parser.add_argument('-model',type=str) # model to reprune
+parser.add_argument('-proto',type=str) # prototxt for reprune model
+parser.add_argument('-mask',type=str) # mask for reprune model
 args = vars(parser.parse_args())
 
 # Path
-loadModel = '/tmp3/joel1211/static_dropout/model/caffenet/bvlc_reference_caffenet.caffemodel'
-prototxt = '/tmp3/joel1211/static_dropout/model/caffenet/deploy.prototxt'
-#loadModel = '/home/master/02/joel1211/git_project/diverse-dropout/chao_test_sdfc_5_5_5_iter_40000.caffemodel'
-#prototxt = '/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_test_sdfc_5_5_5/train_val.prototxt'
+#loadModel = '/tmp3/joel1211/static_dropout/model/caffenet/bvlc_reference_caffenet.caffemodel'
+#prototxt = '/tmp3/joel1211/static_dropout/model/caffenet/deploy.prototxt'
+loadModel = args['model'] #'/home/master/02/joel1211/git_project/diverse-dropout/chao_test_sdfc_5_5_5_iter_40000.caffemodel'
+prototxt = args['proto'] #'/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_test_sdfc_5_5_5/train_val.prototxt'
 
 # Load model
 net = caffe.Net(prototxt, loadModel, caffe.TEST)
@@ -31,6 +34,15 @@ print 'Load from {}'.format(loadModel)
 fc_name = args['name']
 ratio = args['ratio']
 out_file = args['out']
+origin_mask = args['mask']
+
+with open(origin_mask) as f:
+    array = []
+    for line in f: # read rest of lines
+        array = [int(x) for x in line.split()]
+del array[0] # Remove network input size
+del array[0] # Remove network output size
+mask_array = np.array(array)
 
 #tmp_params = {fc_name: (net.params[fc_name][0].data, net.params[fc_name][1].data)}
 #fc_weight = net.params[fc_name][0].data
@@ -38,15 +50,18 @@ out_file = args['out']
 #fc_bias = net.params[fc_name][1].data
 #fc_bias[np.absolute(fc_bias) < th] = 0
 mask_weight = net.params[fc_name][0].data
-sort_weight = np.sort(np.absolute(mask_weight.flatten()));
+w, h = mask_weight.shape
+mask_weight = np.multiply(mask_weight.flatten(), mask_array)
+sort_weight = np.sort(np.absolute(mask_weight));
 th = sort_weight[int(ratio*sort_weight.size)]
 print th
 
-mask_weight[np.absolute(mask_weight) < th] = 0
-mask_weight[np.absolute(mask_weight) >= th] = 1
+mask_weight[np.absolute(mask_weight) <= th] = 0
+mask_weight[np.absolute(mask_weight) > th] = 1
+
 
 with open(out_file, 'w') as f:
-    f.write('{0} {1} '.format(mask_weight.shape[1], mask_weight.shape[0]))
+    f.write('{0} {1} '.format(w, h))
     for n in mask_weight.flatten():
         f.write('{0:.0f} '.format(n))
 
--- script/train_fine.sh
+++ script/train_fine.sh
@@ -1,5 +1,5 @@
 ~/caffe/build/tools/caffe train \
--weights=/home/master/02/joel1211/git_project/diverse-dropout/caffe_90000.caffemodel \
--solver=/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_test_sdfc_5_5_5/solver.prototxt \
--gpu=1 \
-2>&1 | tee /home/master/02/joel1211/git_project/diverse-dropout/log/chao_test_sdfc_5_5_5.log
+-weights=/home/master/02/joel1211/git_project/diverse-dropout/chaonet_5_5_5_50000.caffemodel \
+-solver=/home/master/02/joel1211/git_project/diverse-dropout/zoo/chao_reprune_5_5_5_8_8_8/solver.prototxt \
+-gpu=3 \
+2>&1 | tee /home/master/02/joel1211/git_project/diverse-dropout/log/chao_reprune_5_5_5_8_8_8.log
